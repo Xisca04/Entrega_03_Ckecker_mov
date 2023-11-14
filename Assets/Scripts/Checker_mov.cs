5a8ede06eb27f7bffc0b4187e56600ab23d33e7d
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2023-11-14 18:55:22 +0100

    click_casilla_posición_a_pinyo

--- Assets/Scripts/Checker_mov.cs
+++ Assets/Scripts/Checker_mov.cs
@@ -7,14 +7,14 @@ public class Checker_mov : MonoBehaviour
 {
     // Checker movement: diagonally-up right or left in one unit
 
-
     private Vector2Int position2D;
     private Vector2Int moveDirection;
     private bool validBox;
-
+    [SerializeField] GameObject player;
     private List<Vector2> validBoxList;
     private Vector2 clickedPos;
 
+    // UI Elements
     [SerializeField] private GameObject warningPanel;
     [SerializeField] private Button clueButton;
     [SerializeField] private Button tryAgainButton;
@@ -22,8 +22,8 @@ public class Checker_mov : MonoBehaviour
     private void Awake()
     {
         // Initial position
-        position2D = Vector2Int.zero;
-        transform.position = new Vector3(position2D.x, position2D.y, 0);
+        //position2D = Vector2Int.zero;
+        //transform.position = new Vector3(position2D.x, position2D.y, 0);
 
         // Initialize the valid box list
         List<Vector2> validBoxList = new List<Vector2>();
@@ -34,26 +34,22 @@ public class Checker_mov : MonoBehaviour
         Hide();
     }
 
-    private void Update()
+    private void OnMouseDown()
     {
-       HandleClick();
-    }
-
-    private void HandleClick()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (validBox)
         {
-            if (validBox)
-            {
-                GameObject player = GameObject.FindWithTag("Player");
-                player.transform.position = new Vector3(clickedPos.x, clickedPos.y, 0);
-                
-            }
-            else if (!validBox)
-            {
-                Show();
-            }
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = new Vector3(1, 1, 0);
         }
+        /*
+        else if (!validBox)
+        {
+           Show();
+        }
+        */
+        player = GameObject.FindWithTag("Player");
+        player.transform.position = new Vector3(1, 1, 0);
+        Debug.Log($"mouseDown");
     }
 
     public void Show()
